9df9c7932dafc93d0f6042cdae84902f9d2f3b05
Garett PHG <suffering.train@yahoo.com>
2024-02-21 00:59:14 +0700

    A fix + something should've be done.
    * Fixed `self.class` into `self:class()` on an assertion function.
    * Similar to modified middleclass (branch: `extra`).

--- stateful.lua
+++ stateful.lua
@@ -31,27 +31,40 @@ local Stateful = {
 -- requires middleclass >2.0
 Stateful.static = {}
 
-local _callbacks = {
-  enteredState = 1,
-  exitedState = 1,
-  pushedState = 1,
-  poppedState = 1,
-  pausedState = 1,
-  continuedState = 1
+local table, error, pairs, tostring, type = table, error, pairs, tostring, type
+
+local _callbacks, _BaseState,
+_addStatesToClass, _getStatefulMethod, _getNewInstanceIndex, _getNewAllocateMethod,
+_modifyInstanceIndex, _getNewSubclassMethod, _modifySubclassMethod, _modifyAllocateMethod,
+_assertType, _assertInexistingState, _assertExistingState, _invokeCallback,
+_getCurrentState, _getStateFromClassByName, _getStateIndexFromStackByName, _getStateName
+
+local __stateStack, __instanceDict, __stateName, __key_index, __type_func, __type_string,
+enteredState, exitedState, pushedState, poppedState, pausedState, continuedState =
+    '__stateStack', '__instanceDict', 'stateName', '__index', 'function', 'string',
+    'enteredState', 'exitedState', 'pushedState', 'poppedState', 'pausedState', 'continuedState'
+
+_callbacks = {
+  [enteredState] = 1,
+  [exitedState] = 1,
+  [pushedState] = 1,
+  [poppedState] = 1,
+  [pausedState] = 1,
+  [continuedState] = 1
 }
 
-local _BaseState = {}
+_BaseState = {}
 
-local function _addStatesToClass(klass, superStates)
+function _addStatesToClass(klass, superStates)
   klass.static.states = {}
   for stateName, state in pairs(superStates or {}) do
-    klass:addState(stateName,state)
+    klass:addState(stateName, state)
   end
 end
 
-local function _getStatefulMethod(instance, name)
+function _getStatefulMethod(instance, name)
   if not _callbacks[name] then
-    local stack = rawget(instance, '__stateStack')
+    local stack = rawget(instance, __stateStack)
     if not stack then return end
     for i = #stack, 1, -1 do
       if stack[i][name] then return stack[i][name] end
@@ -59,26 +72,26 @@ local function _getStatefulMethod(instance, name)
   end
 end
 
-local function _getNewInstanceIndex(prevIndex)
-  if type(prevIndex) == 'function' then
+function _getNewInstanceIndex(prevIndex)
+  if type(prevIndex) == __type_func then
     return function(instance, name) return _getStatefulMethod(instance, name) or prevIndex(instance, name) end
   end
   return function(instance, name) return _getStatefulMethod(instance, name) or prevIndex[name] end
 end
 
-local function _getNewAllocateMethod(oldAllocateMethod)
+function _getNewAllocateMethod(oldAllocateMethod)
   return function(klass, ...)
     local instance = oldAllocateMethod(klass, ...)
-    instance.__stateStack = {}
+    instance[__stateStack] = {}
     return instance
   end
 end
 
-local function _modifyInstanceIndex(klass)
-  klass.__instanceDict.__index = _getNewInstanceIndex(klass.__instanceDict.__index)
+function _modifyInstanceIndex(klass)
+  klass[__instanceDict][__key_index] = _getNewInstanceIndex(klass[__instanceDict][__key_index])
 end
 
-local function _getNewSubclassMethod(prevSubclass)
+function _getNewSubclassMethod(prevSubclass)
   return function(klass, name)
     local subclass = prevSubclass(klass, name)
     _addStatesToClass(subclass, klass.states)
@@ -87,58 +100,58 @@ local function _getNewSubclassMethod(prevSubclass)
   end
 end
 
-local function _modifySubclassMethod(klass)
+function _modifySubclassMethod(klass)
   klass.static.subclass = _getNewSubclassMethod(klass.static.subclass)
 end
 
-local function _modifyAllocateMethod(klass)
+function _modifyAllocateMethod(klass)
   klass.static.allocate = _getNewAllocateMethod(klass.static.allocate)
 end
 
-local function _assertType(val, name, expected_type, type_to_s)
+function _assertType(val, name, expected_type, type_to_s)
   if type(val) ~= expected_type then
     error("Expected " .. name .. " to be of type " ..
-          (type_to_s or expected_type) .. ". Was " ..
-          tostring(val) .. "(" .. type(val) .. ")")
+      (type_to_s or expected_type) .. ". Was " ..
+      tostring(val) .. "(" .. type(val) .. ")")
   end
 end
 
-local function _assertInexistingState(klass, stateName)
+function _assertInexistingState(klass, stateName)
   if klass.states[stateName] ~= nil then
-    error("State " .. tostring(stateName) .. " already exists on " .. tostring(klass) )
+    error("State " .. tostring(stateName) .. " already exists on " .. tostring(klass))
   end
 end
 
-local function _assertExistingState(self, state, stateName)
+function _assertExistingState(self, state, stateName)
   if not state then
-    error("The state " .. stateName .. " was not found in " .. tostring(self.class))
+    error("The state " .. stateName .. " was not found in " .. tostring(self:class()))
   end
 end
 
-local function _invokeCallback(self, state, callbackName, ...)
+function _invokeCallback(self, state, callbackName, ...)
   if state and state[callbackName] then state[callbackName](self, ...) end
 end
 
-local function _getCurrentState(self)
-  return self.__stateStack[#self.__stateStack]
+function _getCurrentState(self)
+  return self[__stateStack][#self[__stateStack]]
 end
 
-local function _getStateFromClassByName(self, stateName)
+function _getStateFromClassByName(self, stateName)
   local state = self:class().static.states[stateName]
   _assertExistingState(self, state, stateName)
   return state
 end
 
-local function _getStateIndexFromStackByName(self, stateName)
-  if stateName == nil then return #self.__stateStack end
+function _getStateIndexFromStackByName(self, stateName)
+  if stateName == nil then return #self[__stateStack] end
   local target = _getStateFromClassByName(self, stateName)
-  for i = #self.__stateStack, 1, -1 do
-    if self.__stateStack[i] == target then return i end
+  for i = #self[__stateStack], 1, -1 do
+    if self[__stateStack][i] == target then return i end
   end
 end
 
-local function _getStateName(self, target)
-  for name,state in pairs(self:class().static.states) do
+function _getStateName(self, target)
+  for name, state in pairs(self:class().static.states) do
     if state == target then return name end
   end
 end
@@ -152,69 +165,67 @@ end
 
 function Stateful.static:addState(stateName, superState)
   superState = superState or _BaseState
-  _assertType(stateName, 'stateName', 'string')
+  _assertType(stateName, __stateName, __type_string)
   _assertInexistingState(self, stateName)
-  self.static.states[stateName] = setmetatable({}, { __index = superState })
+  self.static.states[stateName] = setmetatable({}, { [__key_index] = superState })
   return self.static.states[stateName]
 end
 
 function Stateful:gotoState(stateName, ...)
-
   self:popAllStates(...)
 
   if stateName == nil then
-    self.__stateStack = { }
+    self[__stateStack] = {}
   else
-    _assertType(stateName, 'stateName', 'string', 'string or nil')
+    _assertType(stateName, __stateName, __type_string, 'string or nil')
 
     local newState = _getStateFromClassByName(self, stateName)
-    self.__stateStack = { newState }
-    _invokeCallback(self, newState, 'enteredState', ...)
+    self[__stateStack] = { newState }
+    _invokeCallback(self, newState, enteredState, ...)
   end
-
 end
 
-function Stateful:pushState(stateName,...)
-  local oldState = _getCurrentState(self)
-  _invokeCallback(self, oldState, 'pausedState')
+function Stateful:pushState(stateName, ...)
+  local oldState, newState
+  oldState = _getCurrentState(self)
+  _invokeCallback(self, oldState, pausedState)
 
-  local newState = _getStateFromClassByName(self, stateName)
-  table.insert(self.__stateStack, newState)
+  newState = _getStateFromClassByName(self, stateName)
+  table.insert(self[__stateStack], newState)
 
-  _invokeCallback(self, newState, 'pushedState',...)
-  _invokeCallback(self, newState, 'enteredState',...)
+  _invokeCallback(self, newState, pushedState, ...)
+  _invokeCallback(self, newState, enteredState, ...)
 end
 
 function Stateful:popState(stateName, ...)
-
   local oldStateIndex = _getStateIndexFromStackByName(self, stateName)
-  local oldState
+  local oldState, newState
   if oldStateIndex then
-    oldState = self.__stateStack[oldStateIndex]
+    oldState = self[__stateStack][oldStateIndex]
 
-    _invokeCallback(self, oldState, 'poppedState', ...)
-    _invokeCallback(self, oldState, 'exitedState', ...)
+    _invokeCallback(self, oldState, poppedState, ...)
+    _invokeCallback(self, oldState, exitedState, ...)
 
-    table.remove(self.__stateStack, oldStateIndex)
+    table.remove(self[__stateStack], oldStateIndex)
   end
 
-  local newState = _getCurrentState(self)
+  newState = _getCurrentState(self)
 
   if oldState ~= newState then
-    _invokeCallback(self, newState, 'continuedState', ...)
+    _invokeCallback(self, newState, continuedState, ...)
   end
 end
 
 function Stateful:popAllStates(...)
-  local size = #self.__stateStack
-  for _=1,size do self:popState(nil, ...) end
+  local size = #self[__stateStack]
+  for _ = 1, size do self:popState(nil, ...) end
 end
 
 function Stateful:getStateStackDebugInfo()
-  local info = {}
-  local state
-  for i=#self.__stateStack,1,-1 do
-    state = self.__stateStack[i]
+  local state, info
+  info = {}
+  for i = #self[__stateStack], 1, -1 do
+    state = self[__stateStack][i]
     table.insert(info, _getStateName(self, state))
   end
   return info
